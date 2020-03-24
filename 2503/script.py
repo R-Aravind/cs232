754a770d653a610b28cfe98d1cd337ecc8a027e5
R-Aravind <aravind.ar786@gmail.com>
2020-03-24 19:08:41 +0530

    Added Menu and Date wise Plots

--- 2503/script.py
+++ 2503/script.py
@@ -12,11 +12,34 @@ import matplotlib.pyplot as plt
 
 # CONSUMING THE API FROM coronasafe.network to get live data
 # a GET request is sent to the url which returns a json response
+# This data is provided by the ministry of Health Affairs, India
+#
+
 
 url = "https://volunteer.coronasafe.network/api/reports"
 r = requests.get(url)
 data = r.json()
 data = data['kerala']
+
+
+url = "https://api.rootnet.in/covid19-in/stats/daily"
+r = requests.get(url)
+daily_data = r.json()
+daily_data = daily_data['data']
+
+#  Formated raw json data to lists
+#
+
+dates = []
+totalcases = []
+keralacases = []
+
+for x in daily_data:
+    dates.append(x['day'])
+    totalcases.append(x['summary']['total'])
+    for y in x['regional']:
+        if y['loc'] == 'Kerala':
+            keralacases.append(y['confirmedCasesIndian'])
 
 
 # The response is cleaned and formatted into NumPy arrays of Required shape
@@ -38,18 +61,46 @@ district_data = np.array(district_data)
 district_data = district_data.reshape((14, 7))
 
 df = pd.DataFrame(district_data, index=districts, columns=labels)
-print(df)
 
-# Visualised the Data
-#
 
-plt.figure()
-df.iloc[:, 0].plot(kind='bar', title='Under Observation')
+# MENU
+# and
+# Visualisations using various libraries
 
-plt.figure()
-df.iloc[:, 2].plot(kind='bar', title='Total Hospitalised')
 
-plt.figure()
-df.iloc[:, 4].plot(kind='bar', title='Corona Positive')
+print('\n##  MENU  ##\n')
+print('## 1. Table of District wise data in kerala ')
+print('## 2. District wise charts')
+print('## 3. Date wise plots of Cases in Kerala and India')
 
-plt.show()
+ch = int(input('\n Enter a choice: '))
+
+print('\n')
+if ch == 1:
+    print('## DISTRICT WISE TABLE \n')
+    print(df)
+
+elif(ch == 2):
+    plt.figure()
+    df.iloc[:, 0].plot(kind='bar', title='Under Observation')
+
+    plt.figure()
+    df.iloc[:, 2].plot(kind='bar', title='Total Hospitalised')
+
+    plt.figure()
+    df.iloc[:, 4].plot(kind='bar', title='Corona Positive')
+
+    plt.show()
+
+elif(ch == 3):
+    plt.figure()
+    plt.title('TOTAL CASES IN INDIA [date wise]')
+    plt.plot(np.arange(start=10, stop=10 + len(dates)), totalcases)
+
+    plt.figure()
+    plt.title('TOTAL CASES IN KERALA [date wise]')
+    plt.plot(np.arange(start=10, stop=10 + len(dates)), keralacases)
+    plt.show()
+
+else:
+    print('Wrong Choice')
